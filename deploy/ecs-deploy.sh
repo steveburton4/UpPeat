8dfe1875e64295895bf4497e10bb581a6e38908a
steveburton4 <Steven Burton@purple.local>
2017-08-16 10:06:11 +0100

    Correct double call to ecs-deploy

--- deploy/ecs-deploy.sh
+++ deploy/ecs-deploy.sh
@@ -4,7 +4,7 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   # Deploy only if we're testing the master branch
   if [ "$TRAVIS_BRANCH" == "master" ]; then
     echo "Deploying $TRAVIS_BRANCH on $TASK_DEFINITION"
-    ./deploy/ecs-deploy ecs-deploy -c $CLUSTER_NAME -n $SERVICE_NAME -i ignore
+    ./deploy/ecs-deploy -c $CLUSTER_NAME -n $SERVICE_NAME -i ignore
   else
     echo "Skipping deploy because it's not an allowed branch"
   fi
